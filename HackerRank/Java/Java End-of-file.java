408bb8510bcb83b9d68a748200aaff18a9d0012d
Rakhesh Krishna P <119130841+rakheshkrishna2005@users.noreply.github.com>
2024-07-31 18:52:28 +0530

    Create Java End-of-file.java

---
+++ HackerRank/Java/Java End-of-file.java
@@ -0,0 +1,21 @@
+// Link: https://www.hackerrank.com/challenges/java-end-of-file/problem
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+            
+        int n = 1;
+        while(sc.hasNext()){
+            System.out.println(n++ + " " + sc.nextLine());
+        }
+        sc.close();
+    }
+}
